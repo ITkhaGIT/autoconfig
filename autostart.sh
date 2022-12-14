86d6ece0ebd801126d18b9a7ba0d9ace30186790
IronWillDevops <380671617445r@gmail.com>
2022-12-14 15:42:37 +0200

    update autostart

--- autostart.sh
+++ autostart.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+# Check the script is being run by root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# Detect Debian users running the script with "sh" instead of bash
+if readlink /proc/$$/exe | grep -q "dash"; then
+	echo 'This installer needs to be run with "bash", not "sh".'
+	exit 1
+fi
+
+
+
+Main()
+{
+
+}
+
+Main
